a5e40b1c7e565bc20cbc2b235f9a4fdbbc2b7355
Javaid Ahmad Ganaie <ahmad.ganaie@venturepact.com>
2018-04-30 10:29:49 +0530

    Updated result module

--- src/app/site/components/results/results.component.ts
+++ src/app/site/components/results/results.component.ts
@@ -31,7 +31,7 @@ export class ResultsComponent implements OnInit {
   Statistics: any;
   imgMap: string;
   imgMapC: string;
-  state="Tunjab";
+  state="State";
   totalSeats: number;
   partiesInfo: party[] = [];
   TopPartiesCons: any[] = [];
